c59f05c836bb9f6b774ee4f4c4642339a98a8366
Tyler Garben <tylergarben@gmail.com>
2026-06-06 16:40:43 -0700

    Initial Commit

---
+++ BusinessTracker/BusinessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessTrackerApp.swift
+//  BusinessTracker
+//
+//  Created by Tyler Garben on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BusinessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusinessTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BusinessTracker
+//
+//  Created by Tyler Garben on 6/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
